f377595a0a03295ce763e5460a2fb780fc6e8c58
justfade <johnjlbrantley@gmail.com>
2025-07-15 15:56:54 -0500

    Created missing src/index.css file for Tailwind

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // Make sure this path to your main CSS file is correct
+import App from './App'; // This imports your main App component
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
